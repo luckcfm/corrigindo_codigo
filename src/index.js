30306835f08acee43c90b4f88022ca4767764a3b
Lucas Medeiros <luckcfm@gmail.com>
2022-05-14 08:44:43 -0300

    fixing errors, damn that capital leter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid, validate } = require("uuid");
 
 const app = express();
 
@@ -22,33 +22,38 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-
-  return response.json(repository);
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
+  const { title, url, techs } = request.body;
+  if (!validate(id)) {
+    return response.status(404).json({ error: "Thats no moon!" });
+  }
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+  if (title) {
+    repositories[repositoryIndex].title = title;
+  }
+  if (url)
+    repositories[repositoryIndex].url = url;
+  if (techs)
+    repositories[repositoryIndex].techs = techs;
+  // const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
+  return response.status(201).json({ ...repositories[repositoryIndex] });
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -67,8 +72,8 @@ app.post("/repositories/:id/like", (request, response) => {
   }
 
   const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  repositories[repositoryIndex].likes = likes;
+  return response.json({ likes });
 });
 
 module.exports = app;
